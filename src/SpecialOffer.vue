7d98f57e561073e632b635338e258b8ca91a8dba
martin <m.bazan@ntlworld.com>
2018-02-08 11:58:26 +0000

    Added hurry

--- src/SpecialOffer.vue
+++ src/SpecialOffer.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
-                    <strong>Hurry up and get a discount of 20% by clicking an image!</strong>
+                    <strong>HURRY ALONG and get a discount of 20% by clicking an image!</strong>
                 </div>
             </div>
         </div>
